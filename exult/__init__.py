1db8d3e4df2b4078b36e34f6a89f6b036fc1be50
Michael Hucka <mhucka@caltech.edu>
2022-11-13 10:30:57 -0800

    Add __init__.py

---
+++ exult/__init__.py
@@ -0,0 +1,39 @@
+'''
+Exult: Extract the URLs from your personal Twitter archive
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu>
+
+Copyright
+---------
+
+Copyright (c) 2022 by Michael Hucka and the California Institute of Technology.
+This code is open-source software released under a 3-clause BSD license.
+Please see the file "LICENSE" for more information.
+'''
+
+# Package metadata ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#  ╭────────────────────── Notice ── Notice ── Notice ─────────────────────╮
+#  |    The following values are automatically updated at every release    |
+#  |    by the Makefile. Manual changes to these values will be lost.      |
+#  ╰────────────────────── Notice ── Notice ── Notice ─────────────────────╯
+
+__version__     = '0.0.1'
+__description__ = 'Exult: extract URLs from a Twitter archive'
+__url__         = 'https://github.com/mhucka/exult'
+__author__      = 'Mike Hucka'
+__email__       = 'mhucka@caltech.edu'
+__license__     = 'BSD 3-clause license'
+
+
+# Miscellaneous utilities.
+# .............................................................................
+
+def print_version():
+    print(f'{__name__} version {__version__}')
+    print(f'Authors: {__author__}')
+    print(f'URL: {__url__}')
+    print(f'License: {__license__}')
